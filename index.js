faa796090472ed158372a743542929df5d06de30
david-commit <david.ondiege@students.moringaschool.com>
2022-08-22 11:56:55 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const main = document.getElementById('main');
-main.remove();
+const main = document.getElementById('main')
+main.remove()
 
-
-const body = document.body;
-
-const newHeader = document.createElement('h1');
-newHeader.id = ('victory');
-
-newHeader.textContent = "David is the champion";
-newHeader.innerText = document.getElementById('victory')
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "David is the champion"
  
